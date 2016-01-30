1bd81ce077e46d6580ace60a19536cb285b94f29
Juan Manuel Rodriguez <manuel2489@gmail.com>
2016-01-29 18:27:35 -0600

    Se agrego la funcionalidad de remove en autores

--- routes/autores.js
+++ routes/autores.js
@@ -23,11 +23,17 @@ Metodo para listar todos los usuarios
 */
 
 router.get('/list', function(req, res) {
-  res.sendFile(path.join(__dirname, "../views/autores/list.html"))
+  res.sendFile(path.join(__dirname, "../views/autores/list.html"));
+});
+
+/* GET /autores/mostrar/:id*/
+
+router.get('/mostrar/:id', function(req, res){
+  res.sendFile(path.join(__dirname, "../views/autores/mostrar.html"));
 });
 
 /* POST /procesa_autores */
-router.post('/procesa_autores', function(req, res, next) {
+router.post('/crear', function(req, res, next) {
   // Autor.create(req.body, function (err, post) {
   //   if (err) return next(err);
   //   res.json(post);
@@ -63,6 +69,14 @@ router.get('/:id', function(req, res, next) {
   });
 });
 
-
+//Eliminar un autor
+// GET /autores/eliminar/:id
+router.get('/eliminar/:id', function(req, res, next) {
+	var autor = { _id: req.params.id };
+	mongoose.model('Autor').remove(autor, function (err, docs) {
+  		// docs is an array
+	});
+	res.sendFile(path.join(__dirname, "../views/autores/list.html"));
+});
 
 module.exports = router;
